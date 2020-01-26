3f74b8e2c1c424142b7d06c05f838c0ebeeb0a9e
TheEgid <egid.f1@gmail.com>
2020-01-26 18:32:54 +0300

    Refactor getting names of files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # parse_online_library
+
+Python 3.8 должен быть уже установлен. 
+Затем используйте `pip` (или `pip3`, есть есть конфликт с Python2) для установки зависимостей:
+```
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -6,16 +6,29 @@ from pathvalidate import sanitize_filename
 from utils import get_path, convert_to_jpg, make_soup, make_imageresize
 
 
-def get_book_info(soup):
+# def get_book_info(soup):
+#     elements = list()
+#     try:
+#         tags = soup.find_all({'entry_content': True, 'h1': True, })
+#         [elements.append(element.text) for element in tags]
+#         elements = [el.replace('\xa0', ' ') for el in elements][0]
+#         _elements = ''.join(elements)
+#         book_title, book_author = _elements.split('::')
+#         return {'book_title': book_title.strip(),
+#                 'book_author': book_author.strip()}
+#     except ValueError:
+#         return
+
+
+def get_book_title(soup):
     elements = list()
     try:
         tags = soup.find_all({'entry_content': True, 'h1': True, })
         [elements.append(element.text) for element in tags]
         elements = [el.replace('\xa0', ' ') for el in elements][0]
         _elements = ''.join(elements)
-        book_title, book_author = _elements.split('::')
-        return {'book_title': book_title.strip(),
-                'book_author': book_author.strip()}
+        book_title, _ = _elements.split('::')
+        return book_title.strip().replace('. ', '_').replace(': ', '_')
     except ValueError:
         return
 
@@ -65,21 +78,17 @@ def main():
     start_page = 0
     end_page = 11
 
-    root_link = f"http://tululu.org/"
-
-    for id in range(start_page, end_page, 1):
+    for id in range(start_page, end_page):
         info_link = f"http://tululu.org/b{id}/"
         txt_link = f"http://tululu.org/txt.php?id={id}/"
-
         soup = make_soup(info_link)
-        book_info = get_book_info(soup)
-        if book_info:
-            book_title = book_info['book_title']
-            book_title = book_title.replace('. ', '_').replace(': ', '_')
-            download_txt(url=txt_link, filename=book_title)
+
+        book_title = get_book_title(soup)
+
+        if book_title:
+            download_txt(txt_link, book_title)
             book_img = get_book_image(soup)
-            img_link = f'{root_link}{book_img}'
-            download_img(url=img_link, filename=book_title)
+            download_img(f'http://tululu.org/{book_img}', book_title)
 
 
 if __name__ == '__main__':
