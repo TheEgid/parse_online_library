39d465cb26138b16d6c52c0605a7d468c7bc8d7f
TheEgid <egid.f1@gmail.com>
2020-02-04 00:24:59 +0300

    Add download cooments and genres functions

--- main.py
+++ main.py
@@ -41,6 +41,27 @@ def get_book_image(soup):
         return
 
 
+def get_book_comments(soup, book_title):
+    comments = []
+    try:
+        raw_comments = soup.find_all('div', class_='texts')
+        for comment in raw_comments:
+            comments.extend([x.text for x in comment.find_all('span')])
+        return {book_title: '\n'.join(comments)}
+    except TypeError:
+        return
+
+
+def get_book_genres(soup, book_title):
+    genres = []
+    try:
+        raw_genres = soup.find('span', class_='d_book').find_all('a')
+        [genres.append(r.text) for r in raw_genres]
+        return {book_title: genres}
+    except (TypeError, AttributeError):
+        return
+
+
 def download_img(url, filename, folder='images/'):
     filename = f'{filename}.jpg'
     sanitize_filename(filename)
@@ -81,6 +102,7 @@ def main():
     start_page = 0
     end_page = 11
 
+
     for id in range(start_page, end_page):
         info_link = f"http://tululu.org/b{id}/"
         txt_link = f"http://tululu.org/txt.php?id={id}/"
@@ -89,9 +111,18 @@ def main():
         book_title = get_book_title(soup)
 
         if book_title:
-            download_txt(txt_link, book_title)
-            book_img = get_book_image(soup)
-            download_img(f'http://tululu.org/{book_img}', book_title)
+            #download_txt(txt_link, book_title)
+            #book_img = get_book_image(soup)
+
+            #download_img(f'http://tululu.org/{book_img}', book_title)
+
+            # book_comments = get_book_comments(soup, book_title)
+            # print(book_comments)
+
+            book_genres = get_book_genres(soup, book_title)
+            print(book_genres)
+
+
 
 
 if __name__ == '__main__':
