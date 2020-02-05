596306027166e8ceb2086af50e1188407134512b
TheEgid <40724661+TheEgid@users.noreply.github.com>
2020-02-05 10:55:47 +0300

    Make book hrefs from category

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,34 +1,40 @@
 import requests
-import os
-import logging
-from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
+import lxml
 from urllib.parse import urljoin
-from utils import get_path, convert_to_jpg, make_soup, make_imageresize
 
 
-import pprint
-def get_category_hrefs(soup):
+def make_soup(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+
+def get_book_href(href_block, domain):
     try:
-        table = soup.find_all('table', class_='d_book')
-        pprint.pprint(table)
-        return
-    except (TypeError, AttributeError):
-        return
-
-
-def get_book_href(soup, domain):
-    try:
-        table = soup.find('table', class_='d_book')
-        href = table.find('a').get('href')
+        href = href_block.find('a').get('href')
         book_href = urljoin(domain, href)
         return book_href
     except (TypeError, AttributeError):
         return
 
 
+def get_category_hrefs(soup, domain):
+    category_hrefs = list()
+    try:
+        href_blocks = soup.find_all('table', class_='d_book')
+        for href_block in href_blocks:
+            category_hrefs.append(get_book_href(href_block, domain))
+        return category_hrefs
+    except (TypeError, AttributeError):
+        return
+
+
 domain = r"http://tululu.org/"
 g_link = f"{domain}/l55/"
-soup = make_soup(g_link)
 
-get_category_hrefs(soup)
-#print(get_book_href(soup, domain))
+category_hrefs = get_category_hrefs(make_soup(g_link), domain)
+
+print(category_hrefs)
+print(len(category_hrefs))
